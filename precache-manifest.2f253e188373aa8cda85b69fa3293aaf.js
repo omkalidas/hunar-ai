a4fa14b2fc212789f50a58a7e27589fce3a0705f
omkalidas <othakker@gmail.com>
2024-10-01 14:07:15 +0300

    Updates

---
+++ precache-manifest.2f253e188373aa8cda85b69fa3293aaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2a277c76a7f3ce54ba4e567d8cddbe",
+    "url": "/hunar-ai/index.html"
+  },
+  {
+    "revision": "7c452ce05af00e1532d5",
+    "url": "/hunar-ai/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "88df3a8dd6912d741d1f",
+    "url": "/hunar-ai/static/js/2.8a41a75e.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/hunar-ai/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "7c452ce05af00e1532d5",
+    "url": "/hunar-ai/static/js/main.14293aad.chunk.js"
+  },
+  {
+    "revision": "347aa93809bf3363cb20",
+    "url": "/hunar-ai/static/js/runtime~main.89870434.js"
+  }
+]);
